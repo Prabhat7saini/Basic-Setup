e8bbe36b32d723de6dccda90f8a005b646084210
Prabhat Saini <Prabhat.saini@binmile.com>
2025-09-03 18:57:24 +0530

    change in schema

--- pkg/user/models/user.go
+++ pkg/user/models/user.go
@@ -1,13 +1,17 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
 
 type User struct {
-	ID        int    `gorm:"primaryKey"`
-	UUID      string `gorm:"type:varchar;not null"`
-	Name      *string `gorm:"type:varchar;not null"`
-	Email     string `gorm:"type:varchar;not null"`
-	Password  string `gorm:"type:varchar;not null"`
+	ID        int       `gorm:"primaryKey"`
+	UUID      uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();not null"`
+	Name      *string   `gorm:"type:varchar;not null"`
+	Email     string    `gorm:"type:varchar;not null"`
+	Password  string    `gorm:"type:varchar;not null"`
 	Status    bool
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 	CreatedBy *int
